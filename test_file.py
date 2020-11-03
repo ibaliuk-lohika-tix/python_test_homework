64792c89dac7d4ffa04e4a98c778103e6b7f5282
redwing777 <43532999+redwing777@users.noreply.github.com>
2020-11-03 19:56:40 +0200

    Add files via upload

--- test_file.py
+++ test_file.py
@@ -1,36 +1,51 @@
+import pytest
 from assertpy import assert_that, soft_assertions
 import os.path
 
 
-def file_func(lines_from_end: int, file_path: str = "./file_arg/file_arg"):
+def file_func(lines_from_end: int, file_path: str):
     """
-    Function which will read specified last lines from specified f
-
-    ile, and return them in correct order.
+    Function which will read specified last lines from specified file, and return them in correct order.
     :param lines_from_end: Amount of line you needed.
     :param file_path: Path to file we will use.
     :return:
     """
-    if os.path.isfile(file_path):
-        try:
-            with open(file_path, 'r') as file:
-                return ''.join(line for line in file.readlines()[-int(lines_from_end):]) if lines_from_end else ""
-        except:
-            return(f"Check argument values, expected Int and Str, got {type(lines_from_end)} and {type(file_path)} "
-                   f"instead. With argument values {lines_from_end} and {file_path}")
+    if os.path.isfile(file_path) and isinstance(lines_from_end, (int, float)):
+        with open(file_path, 'r') as file:
+            return {"message":
+                    ''.join(line for line in file.readlines()[-int(lines_from_end):]) if lines_from_end > 0 else ""}
     else:
-        return "File doesn't exist, please check file path."
+        return ({"message": "Check file path or lines_from_end value",
+                 "causedBy": "expected Int and Str, got {type(lines_from_end)} and "
+                 f"{type(file_path)} instead. With argument values lines amount:{lines_from_end} and "
+                 f"path to file:{file_path}"})
 
 
-def test_check_job():
+@pytest.mark.parametrize("lines_from_end, file_path, expected", [
+        (0, "./file_arg/file_arg", ""),
+        (5, "./file_arg/file_arg", "".join("67\n78\n89\n90\n01")),
+        (-5, "./file_arg/file_arg", ""),
+        ('A', "./file_arg/file_arg", "Check file path or lines_from_end value"),
+        (None, "./file_arg/file_arg", "Check file path or lines_from_end value"),
+        (5, "./file_arg/file_arg1111", "Check file path or lines_from_end value"),
+        (5.0, "./file_arg/file_arg", "".join("67\n78\n89\n90\n01")),
+        (5, "", "Check file path or lines_from_end value"),
+        (91111, "./file_arg/file_arg", ''.join("12\n23\n34\n45\n56\n67\n78\n89\n90\n01"))
+        ])
+def test_check_job(lines_from_end, file_path, expected):
+    assert_that(file_func(lines_from_end, file_path).get("message")).is_equal_to(expected)
+
+def test_check_job_second_solution():
     with soft_assertions():
-        assert_that(file_func(0)).is_equal_to("")
-        assert_that(file_func(5)).is_equal_to(''.join("6\n7\n8\n9\n0"))
-        assert_that(file_func(-5)).is_equal_to(''.join("6\n7\n8\n9\n0"))
-        assert_that(file_func('A')).is_equal_to("Check argument values, expected Int and Str, got <class 'str'> and "
-                                                "<class 'str'> instead. With argument values A and ./file_arg/file_arg")
-        assert_that(file_func(None)).is_equal_to("")
-        assert_that(file_func(5, "./file_arg/file_arg1111")).is_equal_to("File doesn't exist, please check file path.")
-        assert_that(file_func(5.0, "./file_arg/file_arg")).is_equal_to(''.join("6\n7\n8\n9\n0"))
-        assert_that(file_func(5, "")).is_equal_to("File doesn't exist, please check file path.")
-        assert_that(file_func(91111)).is_equal_to(''.join("6\n7\n8\n9\n0"))
+        assert_that(file_func(0, "./file_arg/file_arg").get("message")).is_equal_to("")
+        assert_that(file_func(5, "./file_arg/file_arg").get("message")).is_equal_to("".join("67\n78\n89\n90\n01"))
+        assert_that(file_func(-5, "./file_arg/file_arg").get("message")).is_equal_to("")
+        assert_that(file_func('A', "./file_arg/file_arg").get("message")).is_equal_to(
+            "Check file path or lines_from_end value")
+        assert_that(file_func(None, "./file_arg/file_arg").get("message")).is_equal_to("")
+        assert_that(file_func(5, "./file_arg/file_arg1111").get("message")).is_equal_to(
+            "Check file path or lines_from_end value")
+        assert_that(file_func(5.0, "./file_arg/file_arg").get("message")).is_equal_to("".join("67\n78\n89\n90\n01"))
+        assert_that(file_func(5, "").get("message")).is_equal_to("Check file path or lines_from_end value")
+        assert_that(file_func(91111, "./file_arg/file_arg").get("message")).is_equal_to(
+            "".join("12\n23\n34\n45\n56\n67\n78\n89\n90\n01"))
