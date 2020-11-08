472d6bf8e483ad97c06c75e4f96fcbbf51c8fd2b
redwing777 <43532999+redwing777@users.noreply.github.com>
2020-11-08 07:06:11 +0200

    Add files via upload

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,30 +1,10 @@
 import pytest
 from assertpy import assert_that, soft_assertions
-
 from app.app import AppData
-from facade import facade
 
 
-@pytest.mark.parametrize("lines_from_end, file_path, expected", [
-        (0, facade.context.profile.file_path, ""),
-        (1, facade.context.profile.file_path, "01"),
-        (10, facade.context.profile.file_path, "12\n23\n34\n45\n56\n67\n78\n89\n90\n01"),
-        (5, facade.context.profile.file_path, "".join("67\n78\n89\n90\n01")),
-        (-5, facade.context.profile.file_path, ""),
-        ('A', facade.context.profile.file_path, facade.context.profile.error_message),
-        (None, facade.context.profile.file_path, facade.context.profile.error_message),
-        (5, "./file_arg/file_arg1111", facade.context.profile.error_message),
-        (5.0, facade.context.profile.file_path, "".join("67\n78\n89\n90\n01")),
-        (5, "", "".join("67\n78\n89\n90\n01")),
-        (91111, facade.context.profile.file_path, "".join("12\n23\n34\n45\n56\n67\n78\n89\n90\n01")),
-        (None, None, facade.context.profile.error_message),
-        ("", "", facade.context.profile.error_message)
-        ])
-def test_check_job(lines_from_end, file_path, expected):
-    assert_that(AppData().file_func(lines_from_end, file_path).get("message")).is_equal_to(expected)
-
-
-def test_check_job_second_solution():
+# ------------------------fixture god-object solution------------------------
+def test_check_job_second_solution(facade):
     error_message = facade.context.profile.error_message
     with soft_assertions():
         assert_that(AppData().file_func(0, facade.context.profile.file_path).get("message")).is_equal_to("")
@@ -48,6 +28,30 @@ def test_check_job_second_solution():
         assert_that(AppData().file_func(None, None).get("message")).is_equal_to(error_message)
 
 
+# ------------------------god-obj init on import solution------------------------
+from facade import facade
+
+
+@pytest.mark.parametrize("lines_from_end, file_path, expected", [
+        (0, facade.context.profile.file_path, ""),
+        (1, facade.context.profile.file_path, "01"),
+        (10, facade.context.profile.file_path, "12\n23\n34\n45\n56\n67\n78\n89\n90\n01"),
+        (5, facade.context.profile.file_path, "".join("67\n78\n89\n90\n01")),
+        (-5, facade.context.profile.file_path, ""),
+        ('A', facade.context.profile.file_path, facade.context.profile.error_message),
+        (None, facade.context.profile.file_path, facade.context.profile.error_message),
+        (5, "./file_arg/file_arg1111", facade.context.profile.error_message),
+        (5.0, facade.context.profile.file_path, "".join("67\n78\n89\n90\n01")),
+        (5, "", "".join("67\n78\n89\n90\n01")),
+        (91111, facade.context.profile.file_path, "".join("12\n23\n34\n45\n56\n67\n78\n89\n90\n01")),
+        (None, None, facade.context.profile.error_message),
+        ("", "", facade.context.profile.error_message)
+        ])
+def test_check_job(lines_from_end, file_path, expected):
+    assert_that(AppData().file_func(lines_from_end, file_path).get("message")).is_equal_to(expected)
+
+
+# ------------------------god-obj init on import solution------------------------
 # TODO Remove if print aren't mandatory condition
 @pytest.mark.parametrize("lines_from_end, file_path, expected, result", [
         (0, facade.context.profile.file_path, "", facade.context.profile.result),
