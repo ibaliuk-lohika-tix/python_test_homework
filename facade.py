3c9fbcbc5776b7872b03354228b79522d0669799
redwing777 <43532999+redwing777@users.noreply.github.com>
2020-11-08 08:57:27 +0200

    Add files via upload

--- facade.py
+++ facade.py
@@ -8,7 +8,7 @@ class Facade:
     def __init__(self):
         self.context = Context()
         self.context.root_path = os.path.abspath(os.path.dirname(__file__))
-        with open(f"{self.context.root_path}/test_profile.json") as test_profile:
+        with open(f"{self.context.root_path}\\test_profile.json") as test_profile:
             self.context.profile = ContextProfile(**json.load(test_profile))
 
 
